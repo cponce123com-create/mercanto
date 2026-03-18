bb5b61d2530937546e51b8c8e20747f741ea3241
cponce123com-create <cponce123.com@gmail.com>
2026-03-18 14:00:23 -0500

    Add Spanish route support in Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,17 +17,38 @@ import NotFound from "@/pages/NotFound";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/search"} component={Search} />
-      <Route path={"/categories"} component={Categories} />
-      <Route path={"/stores"} component={Stores} />
-      <Route path={"/product/:slug"} component={ProductDetail} />
-      <Route path={"/store/:slug"} component={StoreDetail} />
-      <Route path={"/vendor"} component={VendorDashboard} />
-      <Route path={"/vendor/products"} component={VendorProducts} />
-      <Route path={"/vendor/store"} component={VendorStoreSettings} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
+      <Route path="/" component={Home} />
+
+      {/* Búsqueda */}
+      <Route path="/search" component={Search} />
+
+      {/* Categorías: rutas nuevas + compatibilidad con rutas en español */}
+      <Route path="/categories" component={Categories} />
+      <Route path="/categorias" component={Categories} />
+
+      {/* Tiendas: rutas nuevas + compatibilidad */}
+      <Route path="/stores" component={Stores} />
+      <Route path="/tiendas" component={Stores} />
+
+      {/* Producto */}
+      <Route path="/product/:slug" component={ProductDetail} />
+      <Route path="/producto/:slug" component={ProductDetail} />
+
+      {/* Tienda detalle */}
+      <Route path="/store/:slug" component={StoreDetail} />
+      <Route path="/tienda/:slug" component={StoreDetail} />
+
+      {/* Panel vendedor: rutas nuevas + compatibilidad */}
+      <Route path="/vendor" component={VendorDashboard} />
+      <Route path="/panel-vendedor" component={VendorDashboard} />
+
+      <Route path="/vendor/products" component={VendorProducts} />
+      <Route path="/panel-vendedor/productos" component={VendorProducts} />
+
+      <Route path="/vendor/store" component={VendorStoreSettings} />
+      <Route path="/panel-vendedor/tienda" component={VendorStoreSettings} />
+
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
